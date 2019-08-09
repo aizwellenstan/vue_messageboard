032c88c9b502a56738a2e95b323ac2c89ab327f7
aizwellenstan <aizwellenstan@gmail.com>
2019-08-10 01:34:00 +0900

    startup

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#/">Full Stack Message Board</a>
+      <a class="navbar-brand" href="#/">マシュマロ</a>
     </nav>
     <router-view class="container"/>
   </div>
